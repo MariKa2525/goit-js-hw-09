bfd7feae7b7d1d95144b9f902854557e2e6f9e86
MariKa2525 <113701062+MariKa2525@users.noreply.github.com>
2022-12-14 13:53:09 +0200

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startButtonEl = document.querySelector('button[data-start]');
+const stoptButtonEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+let timerId = null;
+
+startButtonEl.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+    startButtonEl.setAttribute('disabled', 'true');
+});
+
+stoptButtonEl.addEventListener("click", () => {
+    clearInterval(timerId);
+    startButtonEl.removeAttribute('disabled');
+  });
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
